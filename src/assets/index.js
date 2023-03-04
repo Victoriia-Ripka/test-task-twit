b6b40e0aed4b84fd883ca8eb83fec635dc18baf0
Victoriia-Ripka <vikaripka@gmail.com>
2023-03-04 14:15:09 +0100

    Updates

--- src/assets/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Logo } from "./Logo.png";
-export { default as Avatar } from "./Boy.png";
-export { default as Background } from "./picture2.png";
--- src/components/index.js
+++
@@ -1,1 +0,0 @@
-export { default as Twit } from './twitt/twit'
